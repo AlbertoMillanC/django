ec3fa631e708e9d14a9cf824b1dbfca3c02fa7b5
Alberto Millán <90913818+AlbertoMillanC@users.noreply.github.com>
2022-06-26 15:18:46 -0500

    cambios en el html

--- Proyecto1/Proyecto1/views.py
+++ Proyecto1/Proyecto1/views.py
@@ -1,8 +1,24 @@
 # hola mundo en django
+from datetime import date
 from django.http import HttpResponse
+import datetime
 
 def saludo(request):
-    return HttpResponse("Hello word")
+    
+    documento="""
+    <html>
+    <body>
+    <h1>
+    Hello world, welcome to the project. 
+    </h1>
+    <body>
+    <html>"""
+    return HttpResponse(documento)
 
 def despedida(request):
     return HttpResponse("goodbye word")
+
+def fecha(request,):
+    
+    fecha=datetime.datetime.now()
+    
