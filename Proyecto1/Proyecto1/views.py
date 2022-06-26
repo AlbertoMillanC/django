e46f6e2470438e4ab416f8e380dec1d0b9b3dbb2
Alberto Millán <90913818+AlbertoMillanC@users.noreply.github.com>
2022-06-26 18:12:42 -0500

    pasar parametros por url

--- Proyecto1/Proyecto1/views.py
+++ Proyecto1/Proyecto1/views.py
@@ -18,7 +18,25 @@ def saludo(request):
 def despedida(request):
     return HttpResponse("goodbye word")
 
-def fecha(request,):
+def dameFecha(request):
     
-    fecha=datetime.datetime.now()
+    fecha_actual=datetime.datetime.now()
     
+    documento="""
+    <html>
+    <body>
+    <h2>
+    Date and time: %s
+    </h2>
+    <body>
+    <html>""" %fecha_actual 
+    
+    return HttpResponse(documento)
+
+def calculaEdad(request,edad,agno):
+    
+    periodo=agno-2022
+    edadFutura=edad+periodo
+    documento="<html><body><h2>En el año %s tendras %s años" %(agno, edadFutura)
+    
+    return HttpResponse(documento)
